6c283e596c87f1864453c911e129a3047b4c87f8
Alberto MG Saruggia <52190087+AMG22075@users.noreply.github.com>
2023-05-27 23:15:18 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,13 @@
 var d = new Date();
 var n = d.getFullYear();
 document.getElementById('date').innerHTML = n;
+
+// i18n integration
+function updateContent() {
+  const i18nElements = document.getElementsByClassName('i18nelement');
+
+  for (const i18nElement of i18nElements) {
+    const key = i18nElement.getAttribute('data-i18n');
+    i18nElement.innerHTML = i18next.t(key) || i18nElement.innerHTML;
+  }
+}
